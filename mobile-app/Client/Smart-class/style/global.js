3e3cfbc172ae44263ec8331d8c1de5f3f25e8b3e
JenceSon <cumeo2503@gmail.com>
2024-03-23 18:54:20 +0700

    ignore

--- mobile-app/Client/Smart-class/style/global.js
+++ mobile-app/Client/Smart-class/style/global.js
@@ -2,7 +2,6 @@ import { StyleSheet, Text, View } from 'react-native';
 export const globalStyles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
     backgroundColor : '#EFF8FF'
