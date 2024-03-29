0045bfab8d8dc8f35fe249d3cec9c454a40fc161
JimmyMcGil1 <92731328+JimmyMcGil1@users.noreply.github.com>
2024-03-29 07:30:32 +0700

    hot fix

--- mobile-app/Client/Smart-class/features/user/userSlice.js
+++ mobile-app/Client/Smart-class/features/user/userSlice.js
@@ -1,9 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
-const initialState = { id: "1", name: "Nguyễn 'Ego' Khánh Hoà", role: "Sinh viên"  }
+const initialState = { id: "1", name: "Nguyễn 'Ego' Khánh Hoà", role: "Student" }
 const userSlice = createSlice({
     name: "user",
     initialState,
     reducers: {
+        setName: ((state, action) => { return {...state, name: action.payload } }),
     }
 })
+export const {setName} = userSlice.actions;
 export default userSlice.reducer
--- mobile-app/Client/Smart-class/page/login-page/login-func.js
+++ mobile-app/Client/Smart-class/page/login-page/login-func.js
@@ -1,11 +1,5 @@
-import React from "react";
-import { MainTeacher } from "../main-page-teacher/main-page-teacher";
-import { Alert } from "react-native";
-
 export function CheckLogin({bkid},{pwd}){
-    
     //khuong duy note - should fetch user infor from backend api, then update user state from store.js
-    
     
     return null
 }
