3895239e71e06a38a0c9ebfcfb7a789b0b0be6b0
JenceSon <cumeo2503@gmail.com>
2024-03-31 10:56:32 +0700

    color adding

--- mobile-app/Client/Smart-class/style/global.js
+++ mobile-app/Client/Smart-class/style/global.js
@@ -15,4 +15,14 @@ export const colors = {
   btnColor : '#0693F1',
   bgColor : '#EFF8FF',
   headerColor1: 'black',
+
+  //use these for yourself
+  primary100 : '#0074CE',
+  primary80 : '#0693F1',
+  primary60 : '#3DB2FF',
+  primary40 : '#B8E1FF',
+  primary20 : 'EFF8FF',
+  black : 'black',
+  white : 'white',
+
 };
